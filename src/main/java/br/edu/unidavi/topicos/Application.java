96eef8590bf5fb89c476ef5dab84aead86696dc2
leo-s-souza <leo-s-souza@live.com>
2016-12-04 17:36:51 -0200

    Leonardo
    Leonardo

---
+++ src/main/java/br/edu/unidavi/topicos/Application.java
@@ -0,0 +1,17 @@
+package br.edu.unidavi.topicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author marcondes
+ */
+@SpringBootApplication
+public class Application {
+   
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+}
